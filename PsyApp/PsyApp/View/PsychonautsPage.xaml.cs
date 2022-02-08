2926dc1f55ae255f5af6077bb22ba9f2000ca705
Eduardo Reis <eduardoreisdev@gmail.com>
2022-02-07 23:16:24 -0300

    Create base ViewModel

--- PsyApp/PsyApp/View/PsychonautsPage.xaml.cs
+++ PsyApp/PsyApp/View/PsychonautsPage.xaml.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using PsyApp.ViewModel;
 using Xamarin.Forms;
 
 namespace PsyApp.View
 {
     public partial class PsychonautsPage : ContentPage
     {
+        private readonly PsychonautsPageViewModel ViewModel;
+
         public PsychonautsPage()
         {
             InitializeComponent();
+
+            ViewModel = new PsychonautsPageViewModel();
+            BindingContext = ViewModel;
         }
     }
 }
---
+++ PsyApp/PsyApp/ViewModel/PsychonautsPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PsyApp.ViewModel
+{
+    public class PsychonautsPageViewModel : BaseViewModel
+    {
+        public PsychonautsPageViewModel()
+        {
+        }
+    }
+}
