2b1490d1fa532a7b437dbcf9d07f9a424c632330
Eduardo Reis <eduardoreisdev@gmail.com>
2022-02-07 22:47:09 -0300

    Create Models

---
+++ PsyApp/PsyApp/Model/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PsyApp.Model
+{
+    public class Character
+    {
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("img")]
+        public Uri Img { get; set; }
+
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("psiPowers")]
+        public List<Power> powers { get; set; }
+
+        [JsonProperty("__v")]
+        public long V { get; set; }
+    }
+}
---
+++ PsyApp/PsyApp/Model/Power.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PsyApp.Model
+{
+    public class Power
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("img")]
+        public string Img { get; set; }
+
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("__v")]
+        public long V { get; set; }
+    }
+}
