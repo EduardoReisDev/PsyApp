06f541f644b50df307a7493ddd51b82d726898c8
Eduardo Reis <eduardoreisdev@gmail.com>
2022-02-07 22:52:59 -0300

    Create Constants

---
+++ PsyApp/PsyApp/Constant/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace PsyApp.Constant
+{
+    public static class Constants
+    {
+        public static string GetCaracters = "https://psychonauts-api.herokuapp.com/api/characters";
+        public static string GetPowers = "https://psychonauts-api.herokuapp.com/api/powers";
+    }
+}
